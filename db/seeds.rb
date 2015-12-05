f31d0e494826ce63c5b913a3757231472b680b8d
builditdan <daniels111@gmail.com>
2015-12-05 17:50:01 -0500

    Testing adding only unique records

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,16 @@ posts = Post.all
   )
 end
 
+Post.create!(title: 'Dan1', body: 'Dan1 body.')
+Post.create!(title: 'Dan1', body: 'Dan1 body.')
+Post.create!(title: 'Dan1', body: 'Dan1 body.')
+Post.find_or_create_by!(title: 'Dans post on Reddit!', body: 'This is my very first post on Reddit. Is is important to point out that it is unique.')
+Comment.find_or_create_by!(body: 'Dans comment on Reddit for my first post!')
+
+# puts post_record.persisted?
+# puts post_record.new_record?
+# puts post_record.methods
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
