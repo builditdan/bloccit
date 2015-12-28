3182631797eb3fdf414e7c42ce90b54be7727bae
builditdan <daniels111@gmail.com>
2015-12-28 08:15:59 -0500

    Added rails code to send emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 has_many :posts, dependent: :destroy
 has_many :comments, dependent: :destroy
 has_many :votes, dependent: :destroy
+has_many :favorites, dependent: :destroy
 
 #  before_save { self.email = email.downcase }
   before_save {
@@ -33,5 +34,9 @@ has_many :votes, dependent: :destroy
 
   enum role: [:member, :admin, :moderator]
 
+  def favorite_for(post)
+    favorites.where(post_id: post.id).first
+  end
+  
 
 end
