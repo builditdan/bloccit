c91e7512a63521f2c9d5f8f813c343217fcbb1ce
builditdan <daniels111@gmail.com>
2016-01-04 15:51:23 -0500

    Added API JSON RESTfull calls for posts and comments

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -10,7 +10,15 @@ class Api::V1::TopicsController < Api::V1::BaseController
 
   def show
     topic = Topic.find(params[:id])
-    render json: topic.to_json, status: 200
+    posts = Post.find(topic.id)
+    # why is the topic header not appearing in the json output,
+    # posts is present with the following "posts":[....]
+    respond_to do |format|
+      format.json  { render json: topic.to_json(include: [:posts])}
+    end
+    # Test call above is curl -H "Authorization: Token 3FqGRrMfvJzUqZTZCwbYubp4c4l99c4rNzqsczhw6Di+ZroPlMlJrP8Iy9zJXCG/QCSJY7b4eKpVB16TAmLNgA==" http://localhost:3000/api/v1/topics/1  
+
+
   end
 
 
